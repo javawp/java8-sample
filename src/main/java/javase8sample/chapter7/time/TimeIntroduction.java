d05e17ee74f6c0536b0cacc1b8c085ab5180dab4
王鹏 <javawp@icloud.com>
2016-07-20 10:13:47 +0800

    java8, 时间安全类, 测试

--- src/main/java/javase8sample/chapter7/time/TimeIntroduction.java
+++ src/main/java/javase8sample/chapter7/time/TimeIntroduction.java
@@ -16,7 +16,12 @@
 package javase8sample.chapter7.time;
 
 
-import java.time.*;
+import java.time.Clock;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.chrono.ChronoLocalDateTime;
 import java.time.chrono.Chronology;
 import java.time.chrono.HijrahChronology;
@@ -25,10 +30,14 @@ import java.time.temporal.IsoFields;
 import java.util.Date;
 
 /**
- *
- * @author benhail
+ * @描述: java8, 时间安全类, 测试
+ * 
+ * @作者: 王鹏
+ * @创建时间: 2016年7月20日-上午10:13:21
+ * @版本: 1.0
  */
 public class TimeIntroduction {
+	
     public static void testClock() throws InterruptedException {
         //时钟提供给我们用于访问某个特定 时区的 瞬时时间、日期 和 时间的。  
         Clock c1 = Clock.systemUTC(); //系统默认UTC时钟（当前瞬时时间 System.currentTimeMillis()）  
@@ -62,32 +71,32 @@ public class TimeIntroduction {
         //使用默认时区时钟瞬时时间创建 Clock.systemDefaultZone() -->即相对于 ZoneId.systemDefault()默认时区  
         LocalDateTime now = LocalDateTime.now();
         System.out.println(now);
-//自定义时区  
+        //自定义时区  
         LocalDateTime now2 = LocalDateTime.now(ZoneId.of("Europe/Paris"));
         System.out.println(now2);//会以相应的时区显示日期  
-//自定义时钟  
+        //自定义时钟  
         Clock clock = Clock.system(ZoneId.of("Asia/Dhaka"));
         LocalDateTime now3 = LocalDateTime.now(clock);
         System.out.println(now3);//会以相应的时区显示日期  
-//不需要写什么相对时间 如java.util.Date 年是相对于1900 月是从0开始  
-//2013-12-31 23:59  
+		//不需要写什么相对时间 如java.util.Date 年是相对于1900 月是从0开始  
+		//2013-12-31 23:59  
         LocalDateTime d1 = LocalDateTime.of(2013, 12, 31, 23, 59);
-//年月日 时分秒 纳秒  
+        //年月日 时分秒 纳秒  
         LocalDateTime d2 = LocalDateTime.of(2013, 12, 31, 23, 59, 59, 11);
-//使用瞬时时间 + 时区  
+        //使用瞬时时间 + 时区  
         Instant instant = Instant.now();
         LocalDateTime d3 = LocalDateTime.ofInstant(Instant.now(), ZoneId.systemDefault());
         System.out.println(d3);
-//解析String--->LocalDateTime  
+        //解析String--->LocalDateTime  
         LocalDateTime d4 = LocalDateTime.parse("2013-12-31T23:59");
         System.out.println(d4);
         LocalDateTime d5 = LocalDateTime.parse("2013-12-31T23:59:59.999");//999毫秒 等价于999000000纳秒  
         System.out.println(d5);
-//使用DateTimeFormatter API 解析 和 格式化  
+        //使用DateTimeFormatter API 解析 和 格式化  
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
         LocalDateTime d6 = LocalDateTime.parse("2013/12/31 23:59:59", formatter);
         System.out.println(formatter.format(d6));
-//时间获取  
+        //时间获取  
         System.out.println(d6.getYear());
         System.out.println(d6.getMonth());
         System.out.println(d6.getDayOfYear());
@@ -97,21 +106,21 @@ public class TimeIntroduction {
         System.out.println(d6.getMinute());
         System.out.println(d6.getSecond());
         System.out.println(d6.getNano());
-//时间增减  
+        //时间增减  
         LocalDateTime d7 = d6.minusDays(1);
         LocalDateTime d8 = d7.plus(1, IsoFields.QUARTER_YEARS);
-//LocalDate 即年月日 无时分秒  
-//LocalTime即时分秒 无年月日  
-//API和LocalDateTime类似就不演示了  
+		//LocalDate 即年月日 无时分秒  
+		//LocalTime即时分秒 无年月日  
+		//API和LocalDateTime类似就不演示了  
     }
     public static void testZonedDateTime() {
         //即带有时区的date-time 存储纳秒、时区和时差（避免与本地date-time歧义）。  
-//API和LocalDateTime类似，只是多了时差(如2013-12-20T10:35:50.711+08:00[Asia/Shanghai])  
+    	//API和LocalDateTime类似，只是多了时差(如2013-12-20T10:35:50.711+08:00[Asia/Shanghai])  
         ZonedDateTime now = ZonedDateTime.now();
         System.out.println(now);
         ZonedDateTime now2 = ZonedDateTime.now(ZoneId.of("Europe/Paris"));
         System.out.println(now2);
-//其他的用法也是类似的 就不介绍了  
+        //其他的用法也是类似的 就不介绍了  
         ZonedDateTime z1 = ZonedDateTime.parse("2013-12-31T23:59:59Z[Europe/Paris]");
         System.out.println(z1);
     }
@@ -119,14 +128,14 @@ public class TimeIntroduction {
     public static void testDuration() {
         //表示两个瞬时时间的时间段  
         Duration d1 = Duration.between(Instant.ofEpochMilli(System.currentTimeMillis() - 12323123), Instant.now());
-//得到相应的时差  
+        //得到相应的时差  
         System.out.println(d1.toDays());
         System.out.println(d1.toHours());
         System.out.println(d1.toMinutes());
         System.out.println(d1.toMillis());
         System.out.println(d1.toNanos());
 
-//1天时差 类似的还有如ofHours()  
+        //1天时差 类似的还有如ofHours()  
         Duration d2 = Duration.ofDays(1);
         System.out.println(d2.toDays());
     }
